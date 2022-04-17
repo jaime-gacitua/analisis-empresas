2917ecb3790a7fdf86acc404063c031679c5b725
Jaime Gacitua <jgacitua@gmail.com>
2022-04-17 12:46:24 -0400

    first commit with cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='APatrones interesantes en empresas constituidas en Chile',
+    author='Jaime Gacitua',
+    license='MIT',
+)
