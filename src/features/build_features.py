7ae518fd756ee7c8090c996e3c7559c0f869430e
Jaime Gacitua <jgacitua@gmail.com>
2022-04-21 11:26:04 -0400

    save processed data and model

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,7 +3,7 @@ from tqdm.notebook import tqdm
 
 
 def remove_stop_phrases(corpus):
-    """Removes a set of phrases that are very common 
+    """Removes a set of phrases that are very common
     in the text and reduce the power of topic modelling.
 
     Parameters
@@ -19,37 +19,37 @@ def remove_stop_phrases(corpus):
     """
 
     REMOVE = ['la empresa tendra por objeto desarrollar las siguientes actividades:',
-                'la sociedad tendra por objeto desarrollar las siguientes actividades:',
-                'la actividad economica que constituira el objeto o el giro de la empresa sera :',
-                'el la sociedad tiene por objeto la',
-                'el objeto de la sociedad sera:',
-                'el objeto de la empresa sera:',
-                'el objeto de es:',
-                'objeto de la sociedad sera',
-                'el objeto de la sociedad es',
-                'la sociedad tendra como objeto unico',
-                'prestacion de',
-                'al por',
-                'el objeto de la empresa sera',
-                'objeto social',
-                'actividad',
-                'actividades',
-                'cualquier otra',
-                'la sociedad tiene por objeto',
-                'en general',
-                'clase de',
-                'cuenta propia',
-                'y de',
-                'todo tipo',
-                'toda',
-                'clase',
-                'relacionadas con',
-                'socios',
-                'podra',
-                'realizar',
-                'la sociedad',
-                'realizacion',
-                'otras']
+              'la sociedad tendra por objeto desarrollar las siguientes actividades:',
+               'la actividad economica que constituira el objeto o el giro de la empresa sera :',
+               'el la sociedad tiene por objeto la',
+               'el objeto de la sociedad sera:',
+               'el objeto de la empresa sera:',
+               'el objeto de es:',
+               'objeto de la sociedad sera',
+               'el objeto de la sociedad es',
+               'la sociedad tendra como objeto unico',
+               'prestacion de',
+               'al por',
+               'el objeto de la empresa sera',
+               'objeto social',
+               'actividad',
+               'actividades',
+               'cualquier otra',
+               'la sociedad tiene por objeto',
+               'en general',
+               'clase de',
+               'cuenta propia',
+               'y de',
+               'todo tipo',
+               'toda',
+               'clase',
+               'relacionadas con',
+               'socios',
+               'podra',
+               'realizar',
+               'la sociedad',
+               'realizacion',
+               'otras']
 
     corpus_out = corpus.copy()
     for s in tqdm(REMOVE):
